c9e99adfa6f5778b0576ec85ae1730218b22bf3d
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-07-12 11:53:28 -0400

    imp search

--- lib/screens/top-lessons.dart
+++ lib/screens/top-lessons.dart
@@ -7,25 +7,29 @@ class TopLessons extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: _title(context),
-        ),
-        body: Row(
-          children: [
-            _search(),
-            _sections()
-          ],
-        ),      
+      ),
+      body: Row(
+        children: [_search(), _sections()],
+      ),
     );
   }
-
 
   Widget _title(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 16),
-      child: Text("Inside Chassidus", style: Theme.of(context).appBarTheme.textTheme.title),
+      child: Text("Inside Chassidus",
+          style: Theme.of(context).appBarTheme.textTheme.title),
     );
   }
 
   Widget _search() {
-    return Container(decoration: BoxDecoration(border: ),)
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 4),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: "Search",
+            suffixIcon: Icon(Icons.search)
+            ),
+        ));
   }
 }
