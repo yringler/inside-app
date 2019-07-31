e270e0452114adb0f8eb3ce8180440ea6b498714
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-07-31 10:11:36 -0400

    add strongly typed child section/lesson methods

--- lib/data/insideData.dart
+++ lib/data/insideData.dart
@@ -23,6 +23,39 @@ class InsideData {
 
   factory InsideData.fromJson(Map<String, dynamic> json) =>
       _$InsideDataFromJson(json);
+
+  /// Returns subsections of the given section.
+  Iterable<SiteSection> getSections(SiteSection section) sync* {
+    if (section?.sectionIds?.isEmpty ?? true) {
+      return;
+    }
+
+    for (var childId in section.sectionIds) {
+      yield sections[childId];
+    }
+  }
+
+  /// Returns all lessons of given section.
+  Iterable<Lesson> getLessons(SiteSection section) sync* {
+    if (section?.lessonIds?.isEmpty ?? true) {
+      return;
+    }
+
+    for (var lessonId in section.lessonIds) {
+      yield lessons[lessonId];
+    }
+  }
+
+  /// Returns all top level sections.
+  Iterable<SiteSection> getTopLevel() sync* {
+    if (topLevel?.isEmpty ?? true) {
+      return;
+    }
+
+    for (var childId in topLevel) {
+      yield sections[childId];
+    }
+  }
 }
 
 @JsonSerializable(fieldRename: FieldRename.pascal)
@@ -54,7 +87,7 @@ class Lesson extends InsideDataBase {
   Lesson(
       {this.id, this.audio, String title, String description, List<String> pdf})
       : super(title: title, description: description, pdf: pdf);
-      
+
   factory Lesson.fromJson(Map<String, dynamic> json) => _$LessonFromJson(json);
 }
 
@@ -62,7 +95,7 @@ class Lesson extends InsideDataBase {
 class Media extends InsideDataBase {
   String source;
 
-  Media({this.source, title, description, pdf})
+  Media({this.source, String title, String description, List<String> pdf})
       : super(title: title, description: description, pdf: pdf);
 
   factory Media.fromJson(Map<String, dynamic> json) => _$MediaFromJson(json);
