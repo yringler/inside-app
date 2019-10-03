083c083785db205cc0d492f091f6e0a9b3c1a80e
Yehuda Ringler <guitarguy613@gmail.com>
2019-10-02 22:12:41 -0400

    format

---
+++ lib/screens/lesson-list/play-button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlayButton extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class _PlayButtonState extends State<PlayButton> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
