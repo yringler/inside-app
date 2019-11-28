733d08aee59caa30c88875a7b31fb839a1cea7d8
Yehuda Ringler <guitarguy613@gmail.com>
2019-11-28 05:55:12 -0500

    #28: Add basic boiler plate for audio_service

--- android/app/src/main/java/com/example/inside_chassidus/MainActivity.java
+++ android/app/src/main/java/com/example/inside_chassidus/MainActivity.java
@@ -3,11 +3,17 @@ package org.insidechassidus.inside_chassidus;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.ryanheise.audioservice.AudioServicePlugin;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    AudioServicePlugin.setPluginRegistrantCallback(this);
+  }
+
+  @Override
+  public void registerWith(PluginRegistry registry) {
+    GeneratedPluginRegistrant.registerWith(registry);
   }
 }
