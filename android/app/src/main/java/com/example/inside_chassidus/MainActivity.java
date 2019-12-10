de25468d7d5bc5320f9acffc3206a6cfc2671ac0
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-12-10 10:52:10 -0500

    #28: android builds

--- android/app/src/main/java/com/example/inside_chassidus/MainActivity.java
+++ android/app/src/main/java/com/example/inside_chassidus/MainActivity.java
@@ -3,17 +3,11 @@ package org.insidechassidus.inside_chassidus;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import com.ryanheise.audioservice.AudioServicePlugin;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    AudioServicePlugin.setPluginRegistrantCallback(this);
-  }
-
-  @Override
-  public void registerWith(PluginRegistry registry) {
-    GeneratedPluginRegistrant.registerWith(registry);
+    GeneratedPluginRegistrant.registerWith(this);
   }
 }
