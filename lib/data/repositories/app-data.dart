8b754f510017904ae298dbfc9b9b867a2e500e7f
Yehuda Ringler <yrappdev@gmail.com>
2020-06-17 09:45:46 -0400

    Toggle version, use git for just_audio_service

--- lib/data/repositories/app-data.dart
+++ lib/data/repositories/app-data.dart
@@ -13,7 +13,7 @@ import 'package:path_provider/path_provider.dart';
 
 /// An entry point into all saved state in the app.
 class AppData {
-  static const dataTypeVersion = 10;
+  static const dataTypeVersion = 11;
 
   /// Get a list of all primary sections.
   Future<List<PrimaryInside>> getPrimaryInside() async {
