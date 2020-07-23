09c82b8de20a2e51f60870b233c59776dd861489
Yehuda Ringler <yrappdev@gmail.com>
2020-07-22 22:14:02 -0400

    format

--- lib/main.dart
+++ lib/main.dart
@@ -35,15 +35,14 @@ void main() async {
       home: Scaffold(
     body: Center(
         child: FutureBuilder(
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          completer.complete(snapshot.data);
-        }
+            future: getBoxes(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                completer.complete(snapshot.data);
+              }
 
-        return CircularProgressIndicator();
-      },
-      future: getBoxes(),
-    )),
+              return CircularProgressIndicator();
+            })),
   )));
 
   final siteBoxes = await completer.future;
@@ -58,94 +57,50 @@ void main() async {
   ));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   static FirebaseAnalytics analytics = FirebaseAnalytics();
 
   @override
-  _MyAppState createState() {
-    MyApp.analytics.logAppOpen();
-    return _MyAppState(analytics: analytics);
-  }
-}
-
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  final FirebaseAnalytics analytics;
-
-  _MyAppState({this.analytics});
-
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Inside Chassidus',
-        theme: ThemeData(primarySwatch: Colors.grey),
-        onGenerateRoute: (settings) {
-          WidgetBuilder builder;
+    MyApp.analytics.logAppOpen();
 
-          _setViewedPage(settings);
+    return AudioServiceWidget(
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Inside Chassidus',
+          theme: ThemeData(primarySwatch: Colors.grey),
+          onGenerateRoute: (settings) {
+            WidgetBuilder builder;
 
-          switch (settings.name) {
-            case SecondarySectionRoute.routeName:
-              final Section routeSection = settings.arguments;
-              builder =
-                  (context) => SecondarySectionRoute(section: routeSection);
-              break;
-            case LessonRoute.routeName:
-              final MediaSection lesson = settings.arguments;
-              builder = (context) => LessonRoute(lesson: lesson);
-              break;
-            case TernarySectionRoute.routeName:
-              final Section routeSection = settings.arguments;
-              builder = (context) => TernarySectionRoute(section: routeSection);
-              break;
-            case PlayerRoute.routeName:
-              final Media media = settings.arguments;
-              builder = (context) => PlayerRoute(media: media);
-              break;
-            default:
-              throw ArgumentError("Unknown route: ${settings.name}");
-          }
+            _setViewedPage(settings);
 
-          return MaterialPageRoute(builder: builder);
-        },
-        home: PrimarySectionsRoute());
-  }
+            switch (settings.name) {
+              case SecondarySectionRoute.routeName:
+                final Section routeSection = settings.arguments;
+                builder =
+                    (context) => SecondarySectionRoute(section: routeSection);
+                break;
+              case LessonRoute.routeName:
+                final MediaSection lesson = settings.arguments;
+                builder = (context) => LessonRoute(lesson: lesson);
+                break;
+              case TernarySectionRoute.routeName:
+                final Section routeSection = settings.arguments;
+                builder =
+                    (context) => TernarySectionRoute(section: routeSection);
+                break;
+              case PlayerRoute.routeName:
+                final Media media = settings.arguments;
+                builder = (context) => PlayerRoute(media: media);
+                break;
+              default:
+                throw ArgumentError("Unknown route: ${settings.name}");
+            }
 
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    connect();
-  }
-
-  @override
-  void dispose() {
-    disconnect();
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    switch (state) {
-      case AppLifecycleState.resumed:
-        connect();
-        break;
-      case AppLifecycleState.paused:
-        disconnect();
-        break;
-      default:
-        break;
-    }
-  }
-
-  void connect() async {
-    await AudioService.connect();
-  }
-
-  void disconnect() {
-    AudioService.disconnect();
+            return MaterialPageRoute(builder: builder);
+          },
+          home: PrimarySectionsRoute()),
+    );
   }
 
   /// Send firebase analytics page view event.
