1b1499ded32e891f68541f9abaaaf6df318e675e
Yehuda Ringler <yrappdev@gmail.com>
2020-08-28 15:47:19 -0400

    try fix can update

--- lib/main.dart
+++ lib/main.dart
@@ -209,25 +209,14 @@ class MyAppState extends State<MyApp> {
 Future<SiteBoxes> getBoxes() async {
   final boxPath = await getApplicationDocumentsDirectory();
   final servicePath = '${boxPath.path}/siteservice_hive';
-  Hive.init('${boxPath.path}/insideapp');
 
-  final box = await Hive.openBox('settings');
-
-  // Be careful not to open up data which is no longer valid.
-
-  final appDataVersion = box.get('version', defaultValue: 0);
-  bool hasData = appDataVersion >= dataVersion;
-
-  if (hasData) {
-    hasData = await compute(_ensureDataLoaded, [servicePath]);
-  }
+  final hasData = await compute(_ensureDataLoaded, [servicePath]);
 
   // Only load the huge json file if we don't already have the data.
   if (!hasData) {
     // Assume that the data bundled with the app is of the correct version.
     final rawData = await rootBundle.loadString('assets/site.json');
     await compute(_ensureDataLoaded, [servicePath, rawData]);
-    box.put('version', dataVersion);
   }
 
   return await getSiteBoxesWithData(hivePath: servicePath);
