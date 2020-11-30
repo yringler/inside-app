51385c6718ff292ffd245474d3e66e0bfbbbf33c
Yehuda Ringler <yrappdev@gmail.com>
2020-11-29 20:49:42 -0500

    fix log completed analytics, fix debug emulator x86 build

--- lib/util/audio-service/audio-task.dart
+++ lib/util/audio-service/audio-task.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:dart_extensions/dart_extensions.dart';
@@ -7,6 +8,7 @@ import 'package:just_audio_service/download-manager/download-audio-task.dart';
 
 class LoggingAudioTask extends AudioTaskDecorater {
   final FirebaseAnalytics analytics = FirebaseAnalytics();
+  StreamSubscription<String> _logCompletedSubscription;
 
   LoggingAudioTask()
       : super(
@@ -17,16 +19,22 @@ class LoggingAudioTask extends AudioTaskDecorater {
   Future<void> onStart(Map<String, dynamic> params) async {
     // Log an analytics event when a lesson is finished.
     // Note that if someone listens to the same class 3 times in a row, it is only logged once.
-    final logCompletedSubscription = context.mediaPlayer.playerStateStream
+    _logCompletedSubscription = context.mediaPlayer.playerStateStream
         .where((state) => state.processingState == ProcessingState.completed)
         .map((state) => context.mediaItem.id)
         .distinct()
-        .listen((id) => analytics.logEvent(
-            name: "completed_class",
-            parameters: {"class_source": id?.limitFromEnd(100) ?? ""}));
+        .listen((id) {
+      analytics.logEvent(
+          name: "completed_class",
+          parameters: {"class_source": id?.limitFromEnd(100) ?? ""});
+    });
 
     await super.onStart(params);
+  }
 
-    await logCompletedSubscription.cancel();
+  @override
+  Future<void> onStop() async {
+    await _logCompletedSubscription?.cancel();
+    super.onStop();
   }
 }
