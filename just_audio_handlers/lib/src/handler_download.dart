9f2410b1a00a7baaf43bfd6690c0b82dc9797b4c
Yehuda Ringler <yrappdev@gmail.com>
2021-04-28 00:14:41 -0400

    Initial finish download handling

--- just_audio_handlers/lib/src/handler_download.dart
+++ just_audio_handlers/lib/src/handler_download.dart
@@ -1,16 +1,13 @@
 import 'dart:async';
 
 import 'package:audio_service/audio_service.dart';
+import 'package:rxdart/subjects.dart';
 
 class AudioHandlerDownloader extends CompositeAudioHandler {
   final AudioDownloader downloader;
 
   AudioHandlerDownloader(AudioHandler inner, {required this.downloader})
       : super(inner);
-
-  /// Listen to command to download an audio file.
-  @override
-  Future<dynamic> customAction(String name, Map<String, dynamic>? extras);
 
   @override
   Future<void> prepareFromMediaId(String mediaId,
@@ -37,20 +34,50 @@ class CreatedDownload {
   /// The download destination.
   final Uri downloadedFile;
 
-  CreatedDownload({required this.complete, required this.downloadedFile});
+  final BehaviorSubject<double> progress;
+
+  CreatedDownload(
+      {required this.complete,
+      required this.downloadedFile,
+      required this.progress});
 }
 
 /// Common interface around downloading an audio file and checking state.
 abstract class AudioDownloader {
   /// Returns the path to use as the audio source.
   /// If the file has been downloaded, returns the file path; otherwise, a noop.
-  Future<Uri> getPlaybackUriFromId(String id);
   Future<Uri> getPlaybackUriFromUri(Uri uri);
 
   /// Get progress stream; .5 is 50% complete, 1 when done.
-  Future<Stream<double>> getProgressFromId(String id);
-  Future<Stream<double>> getProgressFromUri(Uri uri);
+  Future<BehaviorSubject<double>> getProgressFromUri(Uri uri);
 
-  Future<CreatedDownload> downloadFromId(String id);
   Future<CreatedDownload> downloadFromUri(Uri uri);
+}
+
+/// Loads all download data on load from flutter_downloader DB.
+/// Keeps all downloaded data in memory, updating as further downloads are made.
+class FlutterDownloaderAudioDownloader extends AudioDownloader {
+  @override
+  Future<CreatedDownload> downloadFromUri(Uri uri) {
+    // TODO: implement downloadFromUri
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Uri> getPlaybackUriFromId(String id) {
+    // TODO: implement getPlaybackUriFromId
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Uri> getPlaybackUriFromUri(Uri uri) {
+    // TODO: implement getPlaybackUriFromUri
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<BehaviorSubject<double>> getProgressFromUri(Uri uri) {
+    // TODO: implement getProgressFromUri
+    throw UnimplementedError();
+  }
 }
