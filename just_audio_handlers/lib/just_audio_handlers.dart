ee407623582fca586a6cb08074ec43c19ed49831
Yehuda Ringler <yrappdev@gmail.com>
2021-07-04 20:12:09 -0400

    add export

--- just_audio_handlers/lib/just_audio_handlers.dart
+++ just_audio_handlers/lib/just_audio_handlers.dart
@@ -1,2 +1,3 @@
 export 'package:just_audio_handlers/src/handler_just_audio.dart';
 export 'package:just_audio_handlers/src/handler_persist_position.dart';
+export 'package:just_audio_handlers/src/handler_download.dart';
