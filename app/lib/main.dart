348bc2378e6c2025d9d5b6e378b330391ee1d191
Yehuda Ringler <yrappdev@gmail.com>
2021-12-26 16:05:21 -0500

    better move back to tab behaviour

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -184,13 +184,11 @@ class MyAppState extends State<MyApp> {
     BlocProvider.getBloc<IsPlayerButtonsShowingBloc>()
         .isOtherButtonsShowing(isShowing: isOnPlayer);
 
-    if (!(_currentTab == TabType.libraryHome || isOnPlayer)) {
+    if (_currentTab != TabType.libraryHome) {
       _previousTab = _currentTab;
       setState(() {
         _currentTab = TabType.libraryHome;
       });
-    } else {
-      _previousTab = null;
     }
   }
 
@@ -303,6 +301,10 @@ class MyAppState extends State<MyApp> {
   }
 
   void _onBottomNavigationTap(intValue) {
+    // We keep track of where user was, so that back button can go across tabs.
+    // This is only cleared when user resets position by pressing on bottom navigation himself.
+    _previousTab = null;
+
     final value = TabType.values[intValue];
 
     // If the home button is pressed when already on home section, we show the
